61c0f71befc7061de25fa26f6264a2eabdf1dcf1
monjurctg <monjurpy.06@gmail.com>
2025-09-25 11:27:41 +0600

    up

--- resources/views/backend/partials/packages_table.blade.php
+++ resources/views/backend/partials/packages_table.blade.php
@@ -16,40 +16,53 @@
 		<tbody>
 			@if (count($datalist) > 0)
 				@foreach($datalist as $row)
-				<tr>
-					<td class="checkboxlist text-center">
-						<input name="item_ids[]" value="{{ $row->id }}" class="tp-checkbox selected_item" type="checkbox">
-					</td>
+					<tr>
+						<td class="checkboxlist text-center">
+							<input name="item_ids[]" value="{{ $row->id }}" class="tp-checkbox selected_item" type="checkbox">
+						</td>
 
-					<td class="text-left">
-						<a href="{{ route('admin.packages.edit', [$row->id]) }}" title="{{ __('Edit') }}">
-							{{ $row->title }}
-						</a>
-					</td>
-
-					<td class="text-center">{{ number_format($row->price, 2) }}</td>
-					<td class="text-center">{{ $row->duration }} {{ __('days') }}</td>
-
-					@if ($row->status == 1)
-						<td class="text-center"><span class="enable_btn">{{ __('Active') }}</span></td>
-					@else
-						<td class="text-center"><span class="disable_btn">{{ __('Inactive') }}</span></td>
-					@endif
-
-					<td class="text-center">{{ $row->created_at->format('d M, Y') }}</td>
-
-					<td class="text-center">
-						<div class="btn-group action-group">
-							<a class="action-btn" href="javascript:void(0);" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								<i class="fa fa-ellipsis-v"></i>
+						<td class="text-left">
+							<a href="{{ route('admin.packages.edit', [$row->id]) }}" title="{{ __('Edit') }}">
+								{{ $row->title }}
 							</a>
-							<div class="dropdown-menu dropdown-menu-right">
-								<a class="dropdown-item" href="{{ route('admin.packages.edit', [$row->id]) }}">{{ __('Edit') }}</a>
-								<a onclick="onDelete({{ $row->id }})" class="dropdown-item" href="javascript:void(0);">{{ __('Delete') }}</a>
+						</td>
+
+						<td class="text-center">{{ number_format($row->price, 2) }}</td>
+						<td class="text-center">{{ $row->duration }} {{ __('days') }}</td>
+
+						@if ($row->status == 1)
+							<td class="text-center"><span class="enable_btn">{{ __('Active') }}</span></td>
+						@else
+							<td class="text-center"><span class="disable_btn">{{ __('Inactive') }}</span></td>
+						@endif
+
+						<td class="text-center">{{ $row->created_at->format('d M, Y') }}</td>
+
+						<td class="text-center">
+							<div class="btn-group action-group">
+								<a class="action-btn" href="javascript:void(0);" data-toggle="dropdown" aria-haspopup="true"
+									aria-expanded="false"><i class="fa fa-ellipsis-v"></i></a>
+								<div class="dropdown-menu dropdown-menu-right">
+									<a onclick="onEdit({{ $row->id }})" class="dropdown-item"
+										href="javascript:void(0);">{{ __('Edit') }}</a>
+									<a onclick="onDelete({{ $row->id }})" class="dropdown-item"
+										href="javascript:void(0);">{{ __('Delete') }}</a>
+								</div>
 							</div>
-						</div>
-					</td>
-				</tr>
+						</td>
+
+						<!-- <td class="text-center">
+								<div class="btn-group action-group">
+									<a class="action-btn" href="javascript:void(0);" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+										<i class="fa fa-ellipsis-v"></i>
+									</a>
+									<div class="dropdown-menu dropdown-menu-right">
+										<a class="dropdown-item" href="{{ route('admin.packages.edit', [$row->id]) }}">{{ __('Edit') }}</a>
+										<a onclick="onDelete({{ $row->id }})" class="dropdown-item" href="javascript:void(0);">{{ __('Delete') }}</a>
+									</div>
+								</div>
+							</td> -->
+					</tr>
 				@endforeach
 			@else
 				<tr>
