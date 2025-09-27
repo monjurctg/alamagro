60a19a68855b18f07221e445f4d1deb659265276
monjurctg <monjurpy.06@gmail.com>
2025-09-27 14:50:20 +0600

    up

--- resources/views/frontend/packages.blade.php
+++ resources/views/frontend/packages.blade.php
@@ -1,4 +1,38 @@
 @extends('layouts.frontend')
+@section('title', __('Home'))
+@php
+$PageVariation = PageVariation();
+$gtext = gtext();
+@endphp
+
+@section('meta-content')
+	<meta name="keywords" content="{{ $gtext['og_keywords'] }}" />
+	<meta name="description" content="{{ $gtext['og_description'] }}" />
+	<meta property="og:title" content="{{ $gtext['og_title'] }}" />
+	<meta property="og:site_name" content="{{ $gtext['site_name'] }}" />
+	<meta property="og:description" content="{{ $gtext['og_description'] }}" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="{{ url()->current() }}" />
+	<meta property="og:image" content="{{ asset('public/media/'.$gtext['og_image']) }}" />
+	<meta property="og:image:width" content="600" />
+	<meta property="og:image:height" content="315" />
+	@if($gtext['fb_publish'] == 1)
+	<meta name="fb:app_id" property="fb:app_id" content="{{ $gtext['fb_app_id'] }}" />
+	@endif
+	<meta name="twitter:card" content="summary_large_image">
+	@if($gtext['twitter_publish'] == 1)
+	<meta name="twitter:site" content="{{ $gtext['twitter_id'] }}">
+	<meta name="twitter:creator" content="{{ $gtext['twitter_id'] }}">
+	@endif
+	<meta name="twitter:url" content="{{ url()->current() }}">
+	<meta name="twitter:title" content="{{ $gtext['og_title'] }}">
+	<meta name="twitter:description" content="{{ $gtext['og_description'] }}">
+	<meta name="twitter:image" content="{{ asset('public/media/'.$gtext['og_image']) }}">
+@endsection
+
+@section('header')
+@include('frontend.partials.header')
+@endsection
 <body class="bg-gray-50">
     <!-- Navigation -->
     <nav class="hero-gradient shadow-sm sticky top-0 z-40">
@@ -587,4 +621,4 @@
     </button>
 </body>
 
-
+@endsection
