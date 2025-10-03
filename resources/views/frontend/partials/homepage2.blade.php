56620455b3e1210800ba793a5673c730cf40f88c
Monjurul Alam <monjurpy.06@gmail.com>
2025-10-03 11:01:29 +0600

    up

--- resources/views/frontend/partials/homepage2.blade.php
+++ resources/views/frontend/partials/homepage2.blade.php
@@ -246,12 +246,13 @@
       <div class="d-flex overflow-auto">
         @foreach ($brands as $brand)
           <div class="card mx-2 flex-shrink-0 d-flex align-items-center justify-content-center" style="width: 100px; height: 100px;">
-           <a href="{{ route('frontend.brand', ['id' => $brand->id, 'title' => $brand->slug]) }}">
-    <img src="{{ asset('public/media/' . $brand->logo) }}" 
+           <a href="{{ route('frontend.brand', ['id' => $brand->id, 'title' => Str::slug($brand->name)]) }}">
+    <img src="{{ asset('public/media/' . $brand->thumbnail) }}" 
          alt="{{ $brand->name }}" 
          class="img-fluid p-2" 
          loading="lazy">
 </a>
+ 
           </div>
         @endforeach
       </div>
