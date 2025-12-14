b82b1b3fad77af9482c6d9088bf8b5a89351699e
monjurctg <monjurpy.06@gmail.com>
2025-12-14 18:04:34 +0600

    Fix 500 error: Force include helpers.php in Base Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,10 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+if (file_exists(app_path('helpers.php'))) {
+    require_once app_path('helpers.php');
+}
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
